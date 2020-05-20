9711fd97d9a80db391dd49950c353751d3e85f99
Cynthia Dominguez <cynthia.dm1216@gmail.com>
2020-05-20 13:32:18 -0500

    added style to result container and imported

--- client/src/components/ResultContainer/index.js
+++ client/src/components/ResultContainer/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import './style.css';
 
 function ResultContainer(){
-  
+return(
+  <div className='result-container'>
+          <div className='form-group'>
+              <label for='resultInput'>Results</label>  
+          </div>
+      
+  </div>
+  );
 }
 
 export default ResultContainer;
--- client/src/pages/SearchBar.js
+++ client/src/pages/SearchBar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import SearchCard from '../components/SearchCard';
+import ResultContainer from '../components/ResultContainer';
 
 function SearchBar() {
     return(
         <div>
             <SearchCard />
-            
+            <ResultContainer />
         </div>
     );
 }
