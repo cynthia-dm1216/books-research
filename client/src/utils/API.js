de7a4ef2cf626d7489a9c12842b1144df6f76d9e
Cynthia Dominguez <cynthia.dm1216@gmail.com>
2020-05-20 14:15:46 -0500

    created resultCard folder and files

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -16,5 +16,30 @@ export default {
   // Saves a book to the database
   saveBook: function(bookData) {
     return axios.post("/api/books", bookData);
-  }
+  };
+
+  getUsersByLanguage: function(language) {
+    return new Promise((resolve, reject) => {
+      axios
+        .get("https://api.github.com/orgs/github/public_members")
+        .then(res => {
+          const users = res.data;
+          const results = users.map(user => {
+            return {
+              login: user.login,
+              image: user.avatar_url,
+              language: language
+            };
+          });
+          resolve(results);
+        })
+        .catch(err => reject(err));
+    });
+  },
+  // Return a Promise to simulate an async call
+  getLanguagesList: function() {
+    return new Promise(resolve => {
+      resolve(languages);
+    });
+  }  
 };
